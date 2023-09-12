64e9896461341c14e675c79c332eb34b50fbe92d
Vikram Atreyapurapu <vikramat@microsoft.com>
2023-09-12 04:19:22 +0000

    Configured code

---
+++ ICM-tool-n/Config.cs
@@ -0,0 +1,63 @@
+﻿using ICM_hackathon_namespace;
+using Microsoft.Azure.Monitoring.DGrep.DataContracts.External;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICM_hackathon_namespace
+{
+    public class Config
+    {   
+        //Endpoints
+        public static Uri MDSEndpoint = new Uri("https://production.diagnostics.monitoring.core.windows.net/");
+        public static Uri DGrepEndpoint = new Uri("https://dgrepv2-frontend-prod.trafficmanager.net");
+
+        //Time
+        public static DateTimeOffset endtime = DateTimeOffset.UtcNow;
+        public static DateTimeOffset starttime = DateTimeOffset.UtcNow.AddDays(-1);
+
+        //Event Filters
+        public const string NamespaceName = "AzureMessagingRuntime";
+        public const string DGrepEventName = "DeploymentUpgradeHistory";
+
+        public static string[] EventFilterNames = { "ScaleUnit", "RoleInstance", "NamespaceName", "OperationResult" };
+        public static List<string>[] EventFilterValues = { ScaleUnitsQ, RoleInstancesQ, NamespaceNamesQ, OpResultsQ };
+
+        public static List<EventFilter> GetEventFilters()
+        {
+            var list = new List<EventFilter> { new EventFilter { NamespaceRegex = NamespaceName, NameRegex = DGrepEventName } };
+            return list;
+        }
+
+        //Identity Filters
+        public enum IdentityFilters
+        {
+            ScaleUnits,
+            RoleInstances,
+            NamespaceNames,
+            OpResults
+        }
+        protected static List<string> ScaleUnitsQ = new List<string> { "prod-dxb20-401" };
+        protected static List<string> RoleInstancesQ = new List<string> { "SBSQT.17" };
+        protected static List<string> NamespaceNamesQ = new List<string> { "vienna-uaenorth-index" };
+        protected static List<string> OpResultsQ = new List<string> { "InternalServerError" };
+
+        public static string[] IdentityFilterNames = { "ScaleUnit", "RoleInstance", "NamespaceName", "OperationResult" };
+        public static List<string>[] IdentityFilterValues = { ScaleUnitsQ, RoleInstancesQ, NamespaceNamesQ, OpResultsQ };
+
+        public static Dictionary<string, List<string>> GetIdentityFilters()
+        {   
+            var dict = new Dictionary<string, List<string>>();
+            foreach(int filter in Enum.GetValues(typeof(IdentityFilters)))
+            {
+                dict[ IdentityFilterNames[filter] ] = IdentityFilterValues[filter];
+            }
+            return dict;
+        }
+
+        
+
+    }
+}
--- ICM-tool-n/Program.cs
+++ ICM-tool-n/Program.cs
@@ -16,233 +16,35 @@
     using System.Net;
     using System.Net.Http;
     using Newtonsoft.Json;
+    using ICM_hackathon_namespace;
 
 
 
     internal class Program
     {
-
-        public static string GetScaleUnit(string serviceNamespace, string type)
-        {
-            if (type == "public")
-            {
-                Ping myPing = new Ping();
-                Console.WriteLine($"{serviceNamespace}.servicebus.windows.net");
-                PingReply reply = myPing.Send($"{serviceNamespace}.servicebus.windows.net", 500);
-                return reply.ToString();
-            }
-            else
-            {
-                return "x";
-            }
-        }
-
-
-
-
-        public static async Task<double[]> getMetrics(Dictionary<string, string[]> dimensionList, string monitoringAccount, string metricNamespace, string metricName, DateTime startTime, DateTime endTime, SamplingType samplingType)
-        {
-            var connectionInfo = new ConnectionInfo();
-            var reader = new MetricReader(connectionInfo);
-            var id = new MetricIdentifier(monitoringAccount, metricNamespace, metricName);
-
-
-
-            Console.WriteLine(id.ToString());
-
-
-
-            var dimensionFilters = new List<DimensionFilter>();
-            foreach (var dimension in dimensionList)
-            {
-                dimensionFilters.Add(DimensionFilter.CreateIncludeFilter(dimension.Key, dimension.Value));
-
-
-
-            }
-
-
-
-
-
-            IQueryResultListV3 results = reader.GetTimeSeriesAsync(
-                id,
-                dimensionFilters,
-                startTime, endTime,
-                new[] { samplingType }
-             ).Result;
-            double[] metrics = new double[] { };
-            foreach (var series in results.Results)
-            {
-                Console.WriteLine(series.ToString());
-                IEnumerable<string> dimensions = series.DimensionList.Select(x => string.Format("[{0}, {1}]", x.Key, x.Value));
-                metrics = series.GetTimeSeriesValues(samplingType);
-
-
-
-
-
-            }
-
-
-
-
-            return metrics;
-
-
-
-        }
-        class SeriesObj
-        {
-
-
-
-            public DateTime timestamp;
-            public double value;
-            public SeriesObj(DateTime timestamp, double value)
-            {
-                this.timestamp = timestamp;
-                this.value = value;
-            }
-
-
-
-
-
-        }
-
-
-
-        public Task downloadETLLogs()
-        {
-            // jarvis actions or sbot
-            // get metadata
-            // use it to get storage account url
-            return new Task(() => { });
-        }
-
-
-
-        public static async Task<string> checkIfHighIncomingRequests(string scaleUnit)
-        {
-            Dictionary<string, string[]> dimensionList = new Dictionary<string, string[]>
-            {
-                { "ScaleUnit", new string[] { scaleUnit } },
-
-
-            };
-            var metrics = await getMetrics(dimensionList, "servicebus", "OperationQoSMetrics", "IncomingRequests", DateTime.UtcNow.AddHours(-3), DateTime.UtcNow, SamplingType.Sum);
-            using (HttpClient client = new HttpClient())
-            {
-                try
-                {
-                    List<SeriesObj> result = new List<SeriesObj>();
-                    for (int i = 0; i < metrics.Length; i++)
-                    {
-                        DateTime currentDateTime = DateTime.Now;
-                        int minutesToSubtract = metrics.Length - i;
-                        DateTime timestamp = currentDateTime.AddMinutes(-minutesToSubtract);
-                        result.Add(new SeriesObj(timestamp, metrics[i]));
-                    }
-
-
-
-                    // Serialize the C# object to JSON
-                    string jsonData = JsonConvert.SerializeObject(result);
-
-
-
-                    // Create a StringContent object with the JSON data
-                    var content = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
-
-
-
-                    // Send a POST request to the specified URL with the JSON data
-                    HttpResponseMessage response = await client.PostAsync("http://127.0.0.1:5000/post_example", content);
-
-
-
-                    // Check if the response status code indicates success (2xx)
-                    if (response.IsSuccessStatusCode)
-                    {
-                        // Read the content of the response as a string
-                        string responseBody = await response.Content.ReadAsStringAsync();
-
-
-
-                        // Print the response content to the console
-                        Console.WriteLine(responseBody);
-
-
-
-
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Request failed with status code: {response.StatusCode}");
-                    }
-                }
-                catch (HttpRequestException e)
-                {
-                    Console.WriteLine($"HttpRequestException: {e.Message}");
-                }
-            }
-            return "";
-        }
-
-
-
         static async Task Main(string[] args)
         {
-            // If query Geneva Metrics INT environment use : var connectionInfo = new ConnectionInfo(MdmEnvironment.Int);
-            var connectionInfo = new ConnectionInfo();
-            Dictionary<string, string[]> dimensionList = new Dictionary<string, string[]>
+            var input = new QueryInput
             {
-                { "ScaleUnit", new string[] { "prod-dxb20-401" } },
+                MdsEndpoint = Config.MDSEndpoint,
+                EventFilters = new List<EventFilter>
+                {
 
-
-            };
-            var metrics = await getMetrics(dimensionList, "servicebus", "OperationQoSMetrics", "IncomingRequests", DateTime.UtcNow.AddHours(-3), DateTime.UtcNow, SamplingType.Sum);
-
-
-
-            var reader = new MetricReader(connectionInfo);
-            //await checkIfHighIncomingRequests("prod-dxb20-401");
-            // Metric name where Account=MetricTeamInternalMetrics, Namespace=PlatformMetrics, Name=\\Memory\\Available MBytes
-            /*var id = new MetricIdentifier("servicebus", "OperationQoSMetrics", "Role");
-
- 
-
-            IEnumerable<DimensionFilter> dimensionFilters = new List<DimensionFilter>
-            {
-                // Filter represents the condition : Region
-                DimensionFilter.CreateIncludeFilter("ScaleUnit", "prod-dxb20-401")
+                    new EventFilter { NamespaceRegex = Config.NamespaceName, NameRegex = Config.DGrepEventName }
+                },
+                IdentityColumns = Config.GetIdentityFilters(),
+                StartTime = Config.starttime,
+                EndTime = Config.endtime
             };
 
- 
-
-            // Get SUM and COUNT data for last 10 minutes for Top 10 timeseries keys
-            // Where (__Role == Hinting || __Role == MetricsStore) AND (Region == "EAST US")
-            // Order by AVERAGE SUM in descending order
-            // Filters dimensions set should match a configured pre-aggregate on the metric
-            // If you are using custom sampling type, just use new SamplingType({SamplingTypeNameString}) 
-            var knownTimeSeriesDefinitions = reader.GetKnownTimeSeriesDefinitionsAsync(
-                id,
-                dimensionFilters,
-                DateTime.UtcNow.AddMinutes(-10),
-                DateTime.UtcNow).Result;
-
- 
-
-            var roleIndex = knownTimeSeriesDefinitions.FirstOrDefault();
-            Console.WriteLine(roleIndex);
-
- 
-
-            foreach (var value in knownTimeSeriesDefinitions)
+            // Using user based authentication
+            using (var client = new DGrepUserAuthClient(Config.DGrepEndpoint))
             {
-                Console.WriteLine("Known timeseries definition: {0}", JsonConvert.SerializeObject(value.DimensionCombination));
-            }*/
+                RowSetResult result = await client.GetRowSetResultAsync(input, new CancellationToken());
+                var status = result.QueryStatus.Status;
+            }
+
         }
     }
 }
+
